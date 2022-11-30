fa113ec2466d04d761bf6cf01211c6d12595b188
Steevan27 <steevanmahen@gmail.com>
2022-11-30 19:56:40 +0530

    steeadded

---
+++ part92_array/src/org/st/testing/son.java
@@ -0,0 +1,29 @@
+package org.st.testing;
+
+
+public class son {
+	
+	public static void main(String[] args) {  //Array 
+		
+		int a[]=new int[6];
+		a[0]=5;
+		a[1]=6;
+		a[2]=7;
+		a[3]=8;
+		a[4]=9;
+		
+
+		System.out.println(a[3]);
+
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
